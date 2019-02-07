7a7645086f0601db140b276826c87116ac9e2d3a
Carolina Knoll <carolinasknoll@gmail.com>
2019-02-06 22:29:47 -0200

    Add regular expressions testing for zeroes and decimal points

--- js/js-calculator.js
+++ js/js-calculator.js
@@ -13,6 +13,27 @@ $('.calculator button').on('click', (e) => {
   }
 
   else {
+    let calculation = numbers.join('');
+
+    if (e.target.textContent === '0') {
+
+      // check if there is already a zero at the start or after the operations [*/+-]
+      let hasZero = /^[0]|[*\/+-][0]$/;
+
+      if (hasZero.test(calculation)) {
+        return;
+      };
+    }
+
+    if (e.target.textContent === '.') {
+      // check if there is already a decimal point at the start or after the operations [*/+-]
+      let hasDecimal = /^(\d+)[.]$|[*\/+-](\d+)[.]$/;
+
+      if (hasDecimal.test(calculation)) {
+        return;
+      }
+    }
+
     numbers.push(e.target.textContent);
     $('#display').text(numbers.join(''));
   }
