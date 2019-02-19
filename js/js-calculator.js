4e2abcfabe6b06df00cbb3073d2e0c24361a7373
Carolina Knoll <carolinasknoll@gmail.com>
2019-02-19 15:18:48 -0300

    Check for repeated operators before calculating result, and describe regular expressions

--- js/js-calculator.js
+++ js/js-calculator.js
@@ -17,21 +17,49 @@ $('.calculator button').on('click', (e) => {
 
     if (e.target.textContent === '0') {
 
-      // check if there is already a zero at the start or after the operations [*/+-]
-      let hasZero = /^[0]|[*\/+-][0]$/;
+      // if zero is clicked, check if there is already a zero at the start or after the operations [*/+-]
+      let checkForForbiddenZeroes = /^[0]$|[*\/+-][0]$/;
 
-      if (hasZero.test(calculation)) {
+      if (checkForForbiddenZeroes.test(calculation)) {
         return;
       };
     }
 
     if (e.target.textContent === '.') {
-      // check if there is already a decimal point at the start or after the operations [*/+-]
-      let hasDecimal = /^(\d+)[.]$|[*\/+-](\d+)[.]$/;
+      // if decimal is clicked, check if there is already a decimal point at the start or after the operations [*/+-]
 
-      if (hasDecimal.test(calculation)) {
+      // /^(\d+)[.]$/ for start of string, followed by 1+ digits and a decimal at end of string
+      // /[*\/+-](\d+)[.]$/ for an operator, followed by 1+ digits and a decimal at end of string
+      // /[.](\d+)$/ for a decimal, followed by 1+ digits at end of string
+      let checkForForbiddenDecimals = /^(\d+)[.]$|[*\/+-](\d+)[.]$|[.](\d+)$/;
+
+      if (checkForForbiddenDecimals.test(calculation)) {
         return;
       }
+    }
+
+    // if an operator is clicked, check for already existing operators
+    let checkForOperators = /[*\/+-]/;
+
+    // check for cases like 5++5, which will give us an eval error
+    // and remove duplicated operators in order to complete the calculus successfully
+    if (checkForOperators.test(e.target.textContent)) {
+      let checkForRepeatedOperators = new RegExp('\\' + e.target.textContent);
+
+      if (checkForRepeatedOperators.test(calculation)) {
+        calculation = calculation.replace(checkForRepeatedOperators, '');
+      }
+
+      // check for cases like 5+/-2, which should consider only last operator entered
+      // remove existing operators before the last operator entered
+      let checkForExistingOperators = /[*\/+-]{2,}/;
+
+      if (checkForExistingOperators.test(calculation)) {
+        calculation = calculation.replace(checkForExistingOperators, '');
+      }
+
+      // ensure eval is calculated based on new cleaned values
+      numbers = calculation.split('');
     }
 
     numbers.push(e.target.textContent);
